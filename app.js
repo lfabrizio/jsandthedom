2cf64b1595eb5c65d8b1cd3dd79ac29179add0f1
Louis fabrizio <louisfabrizio.student@careerdevs.com>
2019-04-13 01:16:13 +0000

    app.js

---
+++ app.js
@@ -0,0 +1,32 @@
+const list = document.querySelector('#movie-list ul');
+const forms = document.forms;
+
+// delete books or movies
+list.addEventListener('click', (e) => {
+  if(e.target.className == 'delete'){
+    const li = e.target.parentElement;
+    li.parentNode.removeChild(li);
+  }
+});
+
+// add movie
+const addForm = forms['add-movie'];
+addForm.addEventListener('submit', function(e){
+  e.preventDefault();
+
+  // create elements
+  const value = addForm.querySelector('input[type="text"]').value;
+  const li = document.createElement('li');
+  const movieName = document.createElement('span');
+  const deleteBtn = document.createElement('span');
+
+  // add text content
+  movieName.textContent = value;
+  deleteBtn.textContent = 'delete';
+
+  // append to DOM
+  li.appendChild(movieName);
+  li.appendChild(deleteBtn);
+  list.appendChild(li);
+  //list.insertBefore(li, list.querySelector('li:first-child'));
+});
